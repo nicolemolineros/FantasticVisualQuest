a7ed467dad51e76c596393f6e8e4feb5df5ceb6f
nicolemolineros <48654860+nicolemolineros@users.noreply.github.com>
2021-10-12 12:12:41 -0500

    New: Reto OpenGL Texturas

---
+++ TexturaOpenGL/OPGMallasObj/Obj3D.h
@@ -0,0 +1,17 @@
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "glsl.h"
+#include "glm/glm.h"
+
+#pragma once
+class Obj3D
+{
+	GLMmodel* objmodel_ptr;
+
+	public:
+		Obj3D();
+		void AbrirMalla(char *malla);
+		void DibujarMalla(float x, float y, float z);
+
+};
+
